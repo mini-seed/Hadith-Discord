132411220fe0991bf0d3245e63d1bc7ee21957fc
Adam Sherif <adammohamedsherif@gmail.com>
2020-12-16 17:26:54 +0200

    added code boilerplate

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('bot is ready!');
+});
+
+client.on('message', message => {
+
+    if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+
+});
+
+client.login('...');
